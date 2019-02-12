4d8397e993ed474df7071fc2b96b04f1d1419618
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2019-02-12 00:04:33 -1000

    added in the help if no argument in CLI

--- client.js
+++ client.js
@@ -9,42 +9,47 @@ const net = require("net");
 //   output: process.stdout
 // });
 const processArg = process.argv;
-const requestURL = processArg[2].split("/");
-const applyHeader = requestURL[1];
-console.log(requestURL);
-const hostHeader = requestURL[0];
-const hashTable = {};
 
-const client = net.createConnection({ port: 8080 }, () => {
-  console.log("port is open!");
-  if (requestURL.length > 1) {
-    client.write(
-      "GET /" +
-        applyHeader +
-        " HTTP/1.1\nHost: " +
-        hostHeader +
-        "\nConnection: Keep-Alive\nAccept:text/html, application/json\n" +
-        Date()
-    );
-  } else {
-    client.write(
-      "GET / HTTP/1.1\nHost: " +
-        hostHeader +
-        "\nConnection: Keep-Alive\nAccept:text/html, application/json\n" +
-        Date()
-    );
-  }
-});
+if (processArg[2] === undefined) {
+  console.log('Please add an argument to end of "client.js"');
+  return;
+} else {
+  const requestURL = processArg[2].split("/");
+  const applyHeader = requestURL[1];
+  console.log(requestURL);
+  const hostHeader = requestURL[0];
+  const hashTable = {};
+  const client = net.createConnection({ port: 8080 }, () => {
+    console.log("port is open!");
+    if (requestURL.length > 1) {
+      client.write(
+        "GET /" +
+          applyHeader +
+          " HTTP/1.1\nHost: " +
+          hostHeader +
+          "\nConnection: Keep-Alive\nAccept:text/html, application/json\n" +
+          Date()
+      );
+    } else {
+      client.write(
+        "GET / HTTP/1.1\nHost: " +
+          hostHeader +
+          "\nConnection: Keep-Alive\nAccept:text/html, application/json\n" +
+          Date()
+      );
+    }
+  });
 
-client.on("data", data => {
-  let parseData = data.toString();
-  parseData = parseData.split("\n\n");
-  hashTable[requestURL] = parseData[0];
-  console.log(parseData[1]);
-});
-client.on("close", () => {
-  console.log("connection closed!");
-});
+  client.on("data", data => {
+    let parseData = data.toString();
+    parseData = parseData.split("\n\n");
+    hashTable[requestURL] = parseData[0];
+    console.log(parseData[1]);
+  });
+  client.on("close", () => {
+    console.log("connection closed!");
+  });
+}
 
 // var net = require("net");
 
